a587783fa9d6a2ce1ac6fd5b561192e902557ef3
Nathan F <nathan@nathanf.co.uk>
2017-10-06 17:15:41 +0100

    Edited Clues Slightly
    More descriptive and all clues now loaded into ClueBot

--- ClueBot.py
+++ ClueBot.py
@@ -75,6 +75,8 @@ async def clue1(ctx):
     await asyncio.sleep(2)
     await client.say("This hunt should be fun, mainly because I tried to cater for everyone.Solve the puzzle to get your next clue, which has something to do with the colour blue. ")
     await client.say("http://nathanf.co.uk/wp-content/uploads/2017/08/logic.png")
+    await client.say("To unlock the next clue, sned an s. command with the people you think committed the crime in alphabetical order and in lower case.")
+    await client.say("For example, if you think robbie and tambry committed the crime, type s.robbietambry")
 
 #clue1.1: Salisbury Park to Skinners' Field
 @client.command(pass_context=True)
@@ -83,7 +85,7 @@ async def gideontambry(ctx):
     await asyncio.sleep(2)
     await client.send_message(ctx.message.author,"Use command clue12 to get your next clue!")
 
-#clue1.2: Salisbury PArk to SKinners' Field
+#clue1.2: Salisbury PArk to Skinners' Field
 @client.command(pass_context=True)
 async def clue12(ctx):
     await client.say("!clear 1")
@@ -105,25 +107,27 @@ async def clue3(ctx):
     await client.say("!clear 1")
     await asyncio.sleep(2)
     await client.send_message(ctx.message.author,"PLACE SNAPCODE HERE")
+    await client.send_message(discord.Object(id="347403986543968267"),"{} just accessed Clue 3!".format(ctx.message.author))
 
 #clue4: Calverley Grounds to The Forum
 @client.command(pass_context=True)
 async def clue4(ctx):
     await client.say("!clear 1")
-    await asyncio.sleep(2)
     await client.send_message(ctx.message.author,"D yhqxh iru pxvlf, zlwk vrqjv vr suhwwb. Exw brx pdb qrw xvh lw, 'Fdxvh wkh exloglqj orrnv vklwwb. (Foxh Surylghg eb Hood)")
+    await client.send_message(discord.Object(id="347403986543968267"),"{} just accessed Clue 4!".format(ctx.message.author))
 
 #clue5: The Forum to Tunbridge Wells Library
 @client.command(pass_context=True)
 async def clue5(ctx):
-	await client.say("Nathan still needs to do this clue... Go nag him.")
+    await client.say("!clear 1")
+	await client.send_message(ctx.message.author,"The next clue is on a website, http://nathanf.co.uk/twcavenger/clue-2 (CBA to change the web address). But it is locked. You need to use your knowledge of alogrithms to obtain the password.")
+    await client.send_message(ctx.message.author,"My brother goes to Boys Grammar. His name is Joel Friend, and he is in Year 8. If he went to SKA, what would his username be? The username is all lowercase.")
+    await client.send_message(discord.Object(id="347403986543968267"),"{} just accessed Clue 5!".format(ctx.message.author))
 
-
-#command5 @client.command(pass_context=True) async def clear(ctx, number):
-    #mgs = []
-    #number = int(number) #Converting the amount of messages to delete to an integer
-    #async for x in client.logs_from(ctx.message.channel, limit = number):
-        #mgs.append(x)
-    #await client.delete_messages(mgs)
+@client.command(pass_context=True)
+async def clue6(ctx):
+    await client.say("!clear 1")
+    await client.send_message(ctx.message.author,"TQ 580 394")
+    await client.send_message(discord.Object(id="347403986543968267"),"{} just accessed Clue 6!".format(ctx.message.author))
 
 client.run("MzM2MDg5NTU1Mjg1NDQyNTYx.DEzTjQ.fL4TLOO_zv8Xu1yImZrG0GYJXRs")
