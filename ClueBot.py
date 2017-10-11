3a8296a8538873506862f4d9444574320a55bc6e
Nathan F <nathan@nathanf.co.uk>
2017-10-11 18:33:30 +0100

    Added verification for clues

--- ClueBot.py
+++ ClueBot.py
@@ -62,40 +62,45 @@ async def woof(ctx):
 #clue1: Salisbury Park to Skinners' Field
 @client.command(pass_context=True)
 async def clue1(ctx):
-    await client.say("!clear 1")
-    await asyncio.sleep(2)
-    await client.say("This hunt should be fun, mainly because I tried to cater for everyone.Solve the puzzle to get your next clue, which has something to do with the colour blue. ")
+    await client.say("This hunt should be fun, mainly because I tried to cater for everyone.Solve the puzzle to get your first clue, which has something to do with the colour blue. ")
     await client.say("http://nathanf.co.uk/wp-content/uploads/2017/08/logic.png")
-    await client.say("To unlock the next clue, sned an s. command with the people you think committed the crime in alphabetical order and in lower case.")
-    await client.say("For example, if you think robbie and tambry committed the crime, type s.robbietambry")
 
 #clue1.1: Salisbury PArk to Skinners' Field
 @client.command(pass_context=True)
-async def clue12(ctx, user):
+async def clue11(ctx):
+    await client.say("Waiting for clearance from @spookydipity...")
+    await client.send_message(discord.Object(id="347403986543968267"),"{} needs permission to access the Skinners' Field clue. React with 👌 to allow it.".format(ctx.message.author))
+    await client.wait_for_reaction(['👌'], message=msg)
     await client.say("!clear 1")
-    await asyncio.sleep(2)
-    await client.send_message(discord.Object(user),"A place to see the opposite to the sea, kept by our sister school under lock and key.")
-    await client.send_message(discord.Object(id="347403986543968267"),"{} just accessed Clue 1.1!".format(user))
+    await client.send_message(ctx.message.author,"A place to see the opposite to the sea, kept by our sister school under lock and key.")
+    await client.send_message(discord.Object(id="347403986543968267"),"{} just accessed Clue 1.1!".format(ctx.message.author))
 
 #clue2: Skinners' Field to TWGSB
 @client.command(pass_context=True)
 async def clue2(ctx):
+    await client.say("Waiting for clearance from @spookydipity...")
+    await client.send_message(discord.Object(id="347403986543968267"),"{} needs permission to access the TWGSB clue. React with 👍 to allow it.".format(ctx.message.author))
+    await client.wait_for_reaction(['👍'], message=msg)
     await client.say("!clear 1")
-    await asyncio.sleep(2)
     await client.send_message(ctx.message.author,"http://spoti.fi/2tAHjHw")
     await client.send_message(discord.Object(id="347403986543968267"),"{} just accessed Clue 2!".format(ctx.message.author))
 
 #clue3: TWGSB to Calverley Grounds
 @client.command(pass_context=True)
 async def clue3(ctx):
+    await client.say("Waiting for clearance from @spookydipity...")
+    await client.send_message(discord.Object(id="347403986543968267"),"{} needs permission to access the Calverley Grounds clue. React with 🏫 to allow it.".format(ctx.message.author))
+    await client.wait_for_reaction(['🏫'], message=msg)
     await client.say("!clear 1")
-    await asyncio.sleep(2)
     await client.send_message(ctx.message.author,"http://www.nathanf.co.uk/wp-content/uploads/2017/10/snapcode.png")
     await client.send_message(discord.Object(id="347403986543968267"),"{} just accessed Clue 3!".format(ctx.message.author))
 
 #clue4: Calverley Grounds to The Forum
 @client.command(pass_context=True)
 async def clue4(ctx):
+    await client.say("Waiting for clearance from @spookydipity...")
+    await client.send_message(discord.Object(id="347403986543968267"),"{} needs permission to access the The Forum clue. React with 😂 to allow it.".format(ctx.message.author))
+    await client.wait_for_reaction(['😂'], message=msg)
     await client.say("!clear 1")
     await client.send_message(ctx.message.author,"D yhqxh iru pxvlf, zlwk vrqjv vr suhwwb. Exw brx pdb qrw xvh lw, 'Fdxvh wkh exloglqj orrnv vklwwb. (Foxh Surylghg eb Hood)")
     await client.send_message(discord.Object(id="347403986543968267"),"{} just accessed Clue 4!".format(ctx.message.author))
@@ -103,6 +108,9 @@ async def clue4(ctx):
 #clue5: The Forum to Tunbridge Wells Library
 @client.command(pass_context=True)
 async def clue5(ctx):
+    await client.say("Waiting for clearance from @spookydipity...")
+    await client.send_message(discord.Object(id="347403986543968267"),"{} needs permission to access the The Library clue. React with 😉 to allow it.".format(ctx.message.author))
+    await client.wait_for_reaction(['😉'], message=msg)
     await client.say("!clear 1")
     await client.send_message(ctx.message.author,"The next clue is on a website, http://nathanf.co.uk/twcavenger/clue-2 (CBA to change the web address). But it is locked. You need to use your knowledge of algorithms to obtain the password.")
     await client.send_message(ctx.message.author,"My brother goes to Boys Grammar. His name is Joel Friend, and he is in Year 8. If he went to SKA, what would his username be? The username is all lowercase.")
@@ -111,6 +119,9 @@ async def clue5(ctx):
 #clue6: Tunbridge Wells Library to Lower Common
 @client.command(pass_context=True)
 async def clue6(ctx):
+    await client.say("Waiting for clearance from @spookydipity...")
+    await client.send_message(discord.Object(id="347403986543968267"),"{} needs permission to access the The Library clue. React with 🐈 to allow it.".format(ctx.message.author))
+    await client.wait_for_reaction(['🐈'], message=msg)
     await client.say("!clear 1")
     await client.send_message(ctx.message.author,"TQ 580 394")
     await client.send_message(discord.Object(id="347403986543968267"),"{} just accessed Clue 6!".format(ctx.message.author))
